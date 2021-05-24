b5f73e28765310d35719896e540b72fa4aa7ec75
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air-3.local>
2021-05-24 12:05:01 +1200

    Next button functionality
    Adding more example jobs and making the next button scroll through them. However, at this point, it does not stop when you reach the end of the list.

--- display frame.py
+++ display frame.py
@@ -1,6 +1,6 @@
-#setting up display frame GUI
-#version 5 (overall)
-#(version 1 of the display frame)
+#getting multiple jobs to work
+#version 6 (overall)
+#(version 2 of the display frame)
 from tkinter import *
 
 VIRUS_RATE = 0.8
@@ -22,12 +22,17 @@ class Job:
 class JobManagementGUI:
     def __init__(self, parent):
         self.job_list = []
-        self.job_list.append(JOB)
+        self.job_list.append(Job(1, "Olivia", 20, True, True, 38, 147.9))
+        self.job_list.append(Job(2, "Niki", 46, False, True, 0, 130.5))
+        self.job_list.append(Job(3, "Alastair", 1, True, True, 50, 150))
+        self.job_list.append(Job(4, "Alex", 4, True, False, 7, 15.6))
         self.logo_img = PhotoImage(file = "logo.gif")
+
+        self.position = 0
         
         self.display_frame = Frame(parent)
 
-        self.display_label = Label(self.display_frame, text = "Displaying Job: {}/{}".format(JOB.num, len(self.job_list)))
+        self.display_label = Label(self.display_frame, text = "Displaying Job: {}/{}".format(self.position + 1, len(self.job_list)))
         self.display_label.grid(row = 0, column = 0, pady = 10)
 
         self.add_but = Button(self.display_frame, text = "New Job")
@@ -36,25 +41,25 @@ class JobManagementGUI:
         disp_num_desc_label = Label(self.display_frame, text = "Job number:")
         disp_num_desc_label.grid(row = 1, column = 0, sticky = E, padx = 10)
 
-        self.job_num_label = Label(self.display_frame, text = JOB.num)
+        self.job_num_label = Label(self.display_frame, text = self.job_list[self.position].num)
         self.job_num_label.grid(row = 1, column = 1, sticky = W, padx = 10)
 
         disp_name_desc_label = Label(self.display_frame, text = "Customer name:")
         disp_name_desc_label.grid(row = 2, column = 0, sticky = E, padx = 10)
 
-        self.name_label = Label(self.display_frame, text = JOB.name)
+        self.name_label = Label(self.display_frame, text = self.job_list[self.position].name)
         self.name_label.grid(row = 2, column = 1, sticky = W, padx = 10)
 
         disp_charge_desc_label = Label(self.display_frame, text = "Job charge:")
         disp_charge_desc_label.grid(row = 3, column = 0, sticky = E, padx = 10)
 
-        self.charge_label = Label(self.display_frame, text = "${:.2f}".format(JOB.charge))
+        self.charge_label = Label(self.display_frame, text = "${:.2f}".format(self.job_list[self.position].charge))
         self.charge_label.grid(row = 3, column = 1, sticky = W, padx = 10)
 
         back_but = Button(self.display_frame, text = "Back")
         back_but.grid(row = 4, column = 0, pady = 10, sticky = W, padx = 25)
 
-        next_but = Button(self.display_frame, text = "Next")
+        next_but = Button(self.display_frame, text = "Next", command = self.next)
         next_but.grid(row = 4, column = 1, pady = 10, sticky = E, padx = 25)
 
         logo = Label(self.display_frame, image = self.logo_img)
@@ -62,12 +67,18 @@ class JobManagementGUI:
 
         self.display_frame.grid(row = 0, column = 0, padx = 5, pady = 5)
 
+    def next(self):
+        self.position += 1
+        self.display_label.configure(text = "Displaying Job: {}/{}".format(self.position + 1, len(self.job_list)))
+        self.job_num_label.configure(text = self.job_list[self.position].num)
+        self.name_label.configure(text = self.job_list[self.position].name)
+        self.charge_label.configure(text = "${:.2f}".format(self.job_list[self.position].charge))
+
+
 
 #main routine
 if __name__ == "__main__":
     #hard coded job for testing
-    JOB = Job(1, "Olivia", 20, True, True, 38, 147.9)
-    
     root = Tk()
     root.title("Job Management Program")
     JobManager = JobManagementGUI(root)
